9cfcdbde4753714bbdfc835302a0417dc1ef6e57
maxum2003la <maxum2003la@gmail.com>
2025-05-19 00:04:52 +0300

    feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { ManifestOptions, VitePWA } from 'vite-plugin-pwa';
+
+const manifest: Partial<ManifestOptions> = {
+  theme_color: '#8936FF',
+  background_color: '#2EC6FE',
+  icons: [
+    { purpose: 'maskable', sizes: '512x512', src: 'icon512_maskable.png', type: 'image/png' },
+    { purpose: 'any', sizes: '512x512', src: 'icon512_rounded.png', type: 'image/png' },
+  ],
+  orientation: 'any',
+  display: 'standalone',
+  lang: 'en-US',
+  name: 'LearnLog',
+  short_name: 'LL',
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{html,css,js,ico,png,svg}'],
+      },
+      manifest,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
